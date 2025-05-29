f3d45234ec14d5129771a24ae11bc098f8f24bab
GouriPrustyCTS <Gouri.PrustyP@cognizant.com>
2025-05-29 23:47:07 +0530

    created new component Header with correct changes(15)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./component/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -17,5 +18,6 @@ export class AppComponent {
 selector ->  When Angular encounters <app-root></app-root> in your index.html (or another component's template), it knows to render this AppComponent there.
 
 imports ->  This property is used in standalone components (which this component appears to be, as it uses imports directly in @Component instead of NgModule). It declares other standalone components, directives, or pipes that this component needs to use in its template.
+if any other components needs to be used inside this current app component then u mention it
 
  */
