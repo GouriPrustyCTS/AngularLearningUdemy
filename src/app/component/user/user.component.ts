6e7d840dc321bfffdeea16797e4ead6464f59104
GouriPrustyCTS <Gouri.PrustyP@cognizant.com>
2025-05-30 17:40:55 +0530

    tried in signals also (32)

--- src/app/component/user/user.component.ts
+++ src/app/component/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component,  Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -13,13 +13,22 @@ export class UserComponent {
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
+
   onSelectUser() {}
 }
 
 
+
 /**
  * @Input() decorator in Angular is used for passing data from a parent component to a child component. - variable name should be same as parent template as it is coming from there. - and then from here goes to the templateURL to render
- * 
+ *
  * avatar!: string;, optional -  you're telling TypeScript, "I know this property (avatar) will eventually be assigned a value, even though I'm not assigning it here in the constructor or at the point of declaration. Trust me, it will be initialized.
+ *
  * 
+ * to use in signals
+ * 
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(() => 'assets/users/' + this.avatar());
  */
