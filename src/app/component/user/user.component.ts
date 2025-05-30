3f5e1ef35023ce9ee676c695d14c8039e97aa17c
GouriPrustyCTS <Gouri.PrustyP@cognizant.com>
2025-05-30 14:22:51 +0530

    changing data when clicked - using signals (28)-

--- src/app/component/user/user.component.ts
+++ src/app/component/user/user.component.ts
@@ -11,7 +11,7 @@ export class UserComponent {
   //selectedUser - by default it is public or if private then it cant be accessed - public property of the class - whatever u declare here will be available to the html template to access this
   selectedUser = signal(DUMMY_USERS[0]); // signals are like useState() hook in react
 
-  // computed() method is used if signal is implemented
+  // computed() method is used if signal is implemented - re compute when there is change noticed
   imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
   // get imagePath() {
