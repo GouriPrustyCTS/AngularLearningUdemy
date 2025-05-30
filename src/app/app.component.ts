45a574ad5868f81409016579a1508b3ee2baa1a2
GouriPrustyCTS <Gouri.PrustyP@cognizant.com>
2025-05-30 11:46:07 +0530

    imported user details and data binding (21)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
+import { HeaderComponent } from "./component/header/header.component";
+import { UserComponent } from "./component/user/user.component";
 
 @Component({
   selector: 'app-root',
---
+++ src/app/component/user/user.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../../util/dummy-users';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+
+  //selectedUser - by default it is public or if private then it cant be accessed - public property of the class - whatever u declare here will be available to the html template to access this
+  selectedUser = DUMMY_USERS[0];
+
+}
