190fbb5eacffff1a4e73c0e64bb4cc78c1bda7c8
GouriPrustyCTS <Gouri.PrustyP@cognizant.com>
2025-05-30 17:17:30 +0530

    required in input decorator (31)

--- src/app/component/user/user.component.ts
+++ src/app/component/user/user.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input() avatar!: string;
+  @Input({required:true}) avatar!: string;  // this means parent must pass this or else warning in the parent template
   @Input() name!: string;
 
   get imagePath() {
@@ -20,6 +20,6 @@ export class UserComponent {
 /**
  * @Input() decorator in Angular is used for passing data from a parent component to a child component. - variable name should be same as parent template as it is coming from there. - and then from here goes to the templateURL to render
  * 
- * avatar!: string;, you're telling TypeScript, "I know this property (avatar) will eventually be assigned a value, even though I'm not assigning it here in the constructor or at the point of declaration. Trust me, it will be initialized.
+ * avatar!: string;, optional -  you're telling TypeScript, "I know this property (avatar) will eventually be assigned a value, even though I'm not assigning it here in the constructor or at the point of declaration. Trust me, it will be initialized.
  * 
  */
