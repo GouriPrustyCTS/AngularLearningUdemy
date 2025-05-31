d8d98a66cc9d071a59e00f1af1d55bc73a530e5a
GouriPrustyCTS <Gouri.PrustyP@cognizant.com>
2025-05-31 16:46:12 +0530

    component to add new task (50)

--- src/app/component/tasks/tasks.component.ts
+++ src/app/component/tasks/tasks.component.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from '../task/task.component';
 import { dummyTasks } from '../../util/dummy-tasks';
+import { NewTaskComponent } from "../new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   @Input({ required: true }) name!: string;
   @Input({ required: true }) userId!: string;
+  isAddingTask = false;
 
   tasks = dummyTasks;
 
@@ -19,8 +21,11 @@ export class TasksComponent {
   }
 
   onCompleteTask(id: string) {
-    console.log("reachedtask id :",id);
-    
+    console.log('reachedtask id :', id);
     this.tasks = this.tasks.filter((t) => t.id !== id);
+  }
+
+  onAddTask() {
+    this.isAddingTask = true;
   }
 }
